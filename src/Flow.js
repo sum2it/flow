58a5a3f9ce3dd35f1da1f9503c59a7e8cb8bcac9
sum2it <sumitsrisumit@gmail.com>
2022-03-08 15:09:13 +0530

    initial commit

---
+++ src/Flow.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+import ReactFlow, {
+  removeElements,
+  addEdge,
+  Background
+} from "react-flow-renderer";
+
+const onNodeDragStop = (event, node) => console.log("drag stop", node);
+const onElementClick = (event, element) => console.log("click", element);
+const onLoad = (reactFlowInstance) => {
+  console.log(reactFlowInstance);
+  reactFlowInstance.fitView();
+};
+
+const initialElements = [
+  {
+    id: "1",
+    type: "input",
+    data: { label: "Create Purchase Order" },
+    position: { x: 5, y: 400 },
+    targetPosition: 'left',
+    sourcePosition: 'right' 
+  },
+  { id: "2", data: { label: "Intake" }, position: { x: 200, y: 400 }, targetPosition: 'left', sourcePosition: 'right' },
+  { id: "3", data: { label: "Finance Approval" }, position: { x: 400, y: 300 }, targetPosition: 'left', sourcePosition: 'right' },
+  { id: "4", data: { label: "Manager Approval" }, position: { x: 400, y: 500 }, targetPosition: 'left', sourcePosition: 'right' },
+  { id: "5", data: { label: "Procurement Approval" }, position: { x: 600, y: 400 }, targetPosition: 'left', sourcePosition: 'right' },
+  { id: "6", data: { label: "Life goes on 🚀" }, position: { x: 800, y: 400 }, targetPosition: 'left', sourcePosition: 'right' },
+  { id: "e1-2", source: "1", target: "2", animated: false, arrowHeadType: "arrow", style: { stroke: "#7B62FF" } },
+  { id: "e2-3", source: "2", target: "3", animated: false, style: { stroke: "#7B62FF" }  },
+  { id: "e2-4", source: "2", target: "4", animated: false, style: { stroke: "#7B62FF" }  },
+  { id: "e3-5", source: "3", target: "5", animated: false, style: { stroke: "#7B62FF" }  },
+  { id: "e4-5", source: "4", target: "5", animated: false, style: { stroke: "#7B62FF" }  },
+  { id: "e5-6", source: "5", target: "6", animated: false, arrowHeadType: "arrowclosed", style: { stroke: "#7B62FF" }  }
+];
+
+const BasicFlow = () => {
+  const [elements, setElements] = useState(initialElements);
+  const onElementsRemove = (elementsToRemove) =>
+    setElements((els) => removeElements(elementsToRemove, els));
+  const onConnect = (params) => setElements((els) => addEdge(params, els));
+
+  return (
+    <ReactFlow
+      elements={elements}
+      onLoad={onLoad}
+      onElementClick={onElementClick}
+      onElementsRemove={onElementsRemove}
+      onConnect={onConnect}
+      onNodeDragStop={onNodeDragStop}
+    >
+      <Background />
+    </ReactFlow>
+  );
+};
+
+export default BasicFlow;
